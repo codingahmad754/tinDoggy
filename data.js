46d46aa63bf145437f5a0590af2ad84b299ac3f6
codingahmad754 <123690219+codingahmad754@users.noreply.github.com>
2023-07-06 12:29:06 -0700

    first and only commit

---
+++ data.js
@@ -0,0 +1,51 @@
+const dogs = [
+    {
+        name: "Major",
+        avatar: "./images/major1.jpeg",
+        age: 25,
+        bio: "Things I like :Art. Literature. Natural wine. Yoga.",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    }, {
+        name: "Bella",
+        avatar: "./images/dog-bella.jpg",
+        age: 43,
+        bio: "Yup, that's my owner. U can meet him if you want",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Teddy",
+        avatar: "./images/dog-teddy.jpg",
+        age: 30,
+        bio: "How you doin?",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Leo",
+        avatar: "./images/leo.jpg",
+        age: 1,
+        bio: "I like long walks on the beach!",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Major",
+        avatar: "./images/major.jpeg",
+        age: 3,
+        bio: "Can I have some cheese?",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Teddy",
+        avatar: "./images/dog-teddy.jpg",
+        age: 30,
+        bio: "How you doin?",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    }
+]
+
+export default dogs
